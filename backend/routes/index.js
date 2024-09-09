980978e62c599432bddbd6a932917cde02b754a2
benAloni <146990293+benAloni@users.noreply.github.com>
2024-09-09 17:53:49 +0300

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,21 +3,20 @@ import { applicationDefault, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { fillUserInfo, verifyAuthentication } from "../controllers/userAuth.js";
 import { getTeams } from "../controllers/teamsData.js";
-import { getEmployees,addEmployees,updateEmployeeVacation } from "../controllers/employeesData.js";
-import { getFoodHoliday } from "../controllers/foodHolidays.js";
-import { addAllNotifications,getAllNotifications } from "../controllers/AllNotificationsData.js";
+import { getEmployees } from "../controllers/employeesData.js";
 import {
   addEvent,
+  // addEventNote,
   getEvents,
-  deleteEvent,
 } from "../controllers/eventsData.js";
 
 const router = express.Router();
 initializeApp({
   //in order to verify user Id tokens
   credential: applicationDefault(),
-  projectId: 'wolbee-444d9'
+  projectId: "wolbee-444d9"
 });
+
 
 const authMiddleware = async (req, res, next) => {
   let { authorization } = req.headers;
@@ -70,24 +69,11 @@ router.post("/login", verifyAuthentication);
 router.get("/teams", getTeams);
 //get all employees
 router.get("/getEmployees", getEmployees);
-//add new employee
-router.post("/addEmployees", addEmployees);
-//add Update employee
-router.post("/updateEmployeeVacation", updateEmployeeVacation);
-//get all food holidays
-router.get("/getFoodHoliday", getFoodHoliday);
 //get all events
 router.get("/getEvents", getEvents);
 //add event to calendar
 router.post("/addEvent", addEvent);
-//add All Notifications
-router.post("/addAllNotifications", addAllNotifications);
-//get All Notifications
-router.get("/getAllNotifications", getAllNotifications);
-// Add delete event route
- router.delete("/deleteEvent/:id", deleteEvent);
 //add event note endpoint
 // router.post("/addEventNote", addEventNote);
-
 
 export default router;
