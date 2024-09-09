feb06c72cd9348045fed40ef3df2fda9f4874a68
benAloni <146990293+benAloni@users.noreply.github.com>
2024-09-09 17:18:41 +0300

    Update EmployeesModel.js

--- backend/models/EmployeesModel.js
+++ backend/models/EmployeesModel.js
@@ -1,124 +1,89 @@
 import mongoose from "mongoose";
 
-
-
 const employeeSchema = new mongoose.Schema({
-    FullName: {
-      type: String,
-      required: [true, "Please provide full name"]
+    fullName: {
+        type: String,
+        required: [true, "please provide full name"]
     },
-    EmployeeOfManagerId: {
-      type: String,
-      required: [true, "Please provide employee of manager id"]
+    employeeOfManagerId: {
+        type: String,
+        required: [true, "please provide employee of manager id"]
     },
-    EmployeeID: { 
-      type: String, 
-      unique: true, 
-      required: [true, "Please provide civil id"], 
+    id: { // Change to civilId?
+        type: String,
+        unique: true,
+        required: [true, "please provide civil id"]
     },
-    Role: {
-      type: String,
-      required: [true, "Please provide role"]
+    role: {
+        type: String,
+        required: [true, "please provide role"]
     },
-    Team: {
-      type: String,
-      required: [true, "Please provide team id"]
+    team: {
+        type: mongoose.Schema.ObjectId,
+        required: [true, "please provide team id"]
     },
     DataOfBirth: {
-      type: Date, 
-      required: [true, "Please provide date of birth"]
+        type: String,
+        required: [true, "please provide date of birth"]
     },
-    Address: {
-      type: String,
-      required: [true, "Please provide place of residence"]
+    PlaceOfResidence: {
+        type: String,
+        required: [true, "please provide place of residence"]
     },
-    Religion : {
-      type: String,
-    },
-    Passport : {
-      type: String,
-    },
-    Gender : {
-      type: String,
-    },
-    State : {
-      type: String,
-    },
-    Country : {
-      type: String,
-    },
-    PinCode : {
-      type: String,
-    },
-    Department  : {
-      type: String,
-    },
-    Designation   : {
-      type: String,
-    },
-    Email : {
-      type: String,
-    },
-    Phone: {
-      type: String,
-    },
-    MaritalStatus: {
-      type: String,
+    FamilyStatus: {
+        type: String,
+        required: [true, "please provide family status"]
     },
     NumOfChildren: {
-      type: Number,
+        type: Number,
+        required: [true, "please provide number of children"]
     },
-    StartDay: {
-      type: Date, 
-      required: [true, "Please provide start date in the company"]
+    Seniority: {
+        type: Number,
+        required: [true, "please provide years in the company"]
     },
     Anniversary: {
-      type: Date, 
+        type: String,
+        required: [true, "please provide anniversary date"]
     },
     LatestActivity: {
-      type: Array,
+        type: Array,
+        required: [true, "please provide latest activity"]
     },
-    InterestingFacts: {
-      type: String,
+    InterestingFact: {
+        type: String,
+        required: [true, "please provide interesting fact"]
     },
     ClosestPersonalEvent: {
-      type: [String],
+        type: Array,
+        required: [true, "please provide closest personal event"]
     },
-    Singers: {
-      type: [String],
+    singers: {
+        type: Array,
+        required: [true, "please provide singers"]
     },
-    FoodAndDrinks: [{
-      food1: { type: String },
-      food2: { type: String },
-      drink: { type: String }
-    }],
-    Restaurants: [{
-      restaurant1: { type: String },
-      restaurant2: { type: String }
-    }],
-    Hobbies: [{
-      hobby1: { type: String },
-      hobby2: { type: String },
-      hobby3: { type: String }
-    }],
+    FoodAndDrinks: {
+        type: Array,
+        required: [true, "please provide food and drinks"]
+    },
+    Restaurants: {
+        type: Array,
+        required: [true, "please provide restaurants"]
+    },
+    Hobbies: {
+        type: Array,
+        required: [true, "please provide hobbies"]
+    },
     TopInsights: {
-      type: Array,
+        type: Array,
+        required: [true, "please provide top insights"]
     },
     LatestInfo: {
-      type: Array,
+        type: Array,
+        required: [true, "please provide latest info"]
     },
-    Vacation: [{
-    name:{type:String},
-    startDate: {type: Date},
-     endDate: {type: Date },
-        }],
-     
-   
-    uid:
-    {type:String} ,
-  });
-  
-  const EmployeeModel = mongoose.model('Employee', employeeSchema);
-  
-  export default EmployeeModel;
-  
+});
+
+const EmployeeModel = mongoose.model('employees', employeeSchema);
+
+export default EmployeeModel;
