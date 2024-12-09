9de0ec6b46871ded41f3bc1428c310bd929e068a
issacGut <159480151+issacGut@users.noreply.github.com>
2024-12-09 12:51:03 +0200

    Update ShowUpcomingEvents.jsx

--- fronted/src/views/pages/Employees/Projects/mainUpcomingEvents/ShowUpcomingEvents.jsx
+++ fronted/src/views/pages/Employees/Projects/mainUpcomingEvents/ShowUpcomingEvents.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Modal from "react-modal";
 
-// imgs12
+// imgs1
 import pizza from "../../../../../imgs/dismissIcon.png";
 
 export default function ShowUpcomingEvents({ closestEvents }) {
